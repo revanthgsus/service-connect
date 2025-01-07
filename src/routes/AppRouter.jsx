e5f36276b95bcf8e844efcde9d96213a2ce3bd3f
Revanth <revanth2899@gmail.com>
2025-01-07 16:07:58 +0530

    new commit

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AuthRouter from './AuthRouter';
 import AdminRouter from './AdminRouter';
 
 const AppRouter = () => {
   return (
-    <Router >
       <Routes>
         <Route path="/*" element={<AuthRouter />} />
         <Route path="/admin/*" element={<AdminRouter />} />
       </Routes>
-    </Router>
   );
 }
 
