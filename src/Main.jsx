a81217371ff6f847c3b4a26f12896ee2a367eb25
Revanth <revanth2899@gmail.com>
2025-01-17 11:27:29 +0530

    update commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppRouter from './routes/AppRouter';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const Main = () => {
   return (
