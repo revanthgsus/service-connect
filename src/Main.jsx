5a49aed1d752afe4cf2ee6fd00c95a59fc6790a9
Revanth <revanth2899@gmail.com>
2025-02-26 14:51:16 +0530

    update commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const Main = () => {
   return (
-    <Router basename='/service-connect'>
+    <Router>
       <AppRouter />
     </Router>
   )
